49794565d1f9bd278ec3488d1ca373b049ce4ea8
Soham <sonawanesoham2002@gmail.com>
2023-08-13 12:04:55 +0530

    Attempted

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,36 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    //--------------------------------- constructor -----------------------------
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public Boat() {
+    }
+
+    //--------------------------------- setters -------------------------------------
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    //------------- getters overloaded from interface ----------------------------
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,17 @@ package com.driver;
 
 public class F1 extends Car {
 
+    //----------------- constructor --------------------
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+//        super(name, isManual);
+        super(name,0,0,0,isManual,"arbitary",0);
     }
 
+
+    //--------------------- other methods -----------------------
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = super.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,14 +22,27 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        if(newSpeed<0){
+            newSpeed = 0;
+        }
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            super.setGears(1);
         }
+
+
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
+            if(newSpeed<=50){ changeGear(1);}
+            else if(newSpeed<=100){ changeGear(2);}
+            else if(newSpeed<=150){ changeGear(3);}
+            else if(newSpeed<=200){ changeGear(4);}
+            else if(newSpeed<=250){ changeGear(5);}
+            else{ changeGear(6); }
         }
     }
 }
